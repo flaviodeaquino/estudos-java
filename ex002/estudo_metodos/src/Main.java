1ec0b4ec90c3b92dfcec74ba2d95603b1aef1226
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-06-17 18:48:46 -0300

    commit inicial

---
+++ ex002/estudo_metodos/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        //Calculadora
+        System.out.println("Exercício Calculadora");
+        Calculadora.soma(3,6);
+        Calculadora.subtração(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println("Exercício Mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+
+        //Empréstimo
+        System.out.println("Exercício Empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+
+    }
+}
+
+
